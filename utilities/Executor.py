fd377e118d265e6909b63d5e80c631c0ae6dafd5
cdywalst <candyahogoudedji@gmail.com>
2023-05-09 09:46:23 +0100

    ajout executor.py

--- utilities/Executor.py
+++ utilities/Executor.py
@@ -1,5 +1,49 @@
+from fastapi import APIRouter, Depends
+
+import osmnx as ox
+import networkx as nx
+
+from model.entity.Incident import Incident
+from model.entity.Vehicle import Vehicle
+from model.enum.AlertKind import AlertKind
+
+
 class Executor:
-    # TODO: Tu dois écrire une méthode pour chaque étape de l'algorithme comme on en parlé (filtrage, tri, sélection et autres)
-    # TODO: Tu vas créer une méthode run qui vas prendre en entrée un incident puis appliquer toutes les étapes de l'algorithme à cet incident
-    # Pour le moment vu que la fonction de Morel n'est pas disponible, tu peux utiliser la fonction de networkx pour calculer le plus court chemin
-    pass
+    all:list[Vehicle]
+    alert:AlertKind
+    def filtre_type_incident(self):
+
+        for vehicle in self.all:
+            if not vehicle.can_support(self.alert):
+                self.all.remove(vehicle)
+        return self.all
+
+    def run(self,incident:Incident):
+        l_v=self.filtre_type_incident()
+        L = []
+        for v in l_v:
+            L.append(v.haversine(incident))
+        d = {}
+        for i in range(len(l_v)):
+            d[l_v[i]] = L[i]
+
+        G = ox.graph_from_place('Khouribga, Maroc', network_type='drive')
+        end = (incident.longitude, incident.latitude)
+        start = [(v.latitude,  v.longitude) for v in all]
+        start_node = ox.distance.nearest_nodes(G, X=[start[0] for start in start], Y=[start[1] for start in start])[0]
+        end_node = ox.distance.nearest_nodes(G, X=[end[0]], Y=[end[1]])[0]
+        shortest_path = nx.shortest_path(G, start_node, end_node, weight='length')
+        ox.plot_graph_route(G, shortest_path)
+        path = [{"latitude": G.nodes[node]['y'], "longitude": G.nodes[node]["x"]} for node in shortest_path]
+
+        return {
+            "success": True,
+            "data": [
+                {
+                    "typeVehicle": "",
+                    "typeIncident": self.alert.value,
+                    "path": path
+
+                }
+            ]}
+
